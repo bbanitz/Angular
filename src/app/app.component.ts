16211a387462a072f8ed1657f505ff9cdf0be0e2
Bernard BANITZ <bernard@banitz.fr>
2017-11-24 18:38:55 +0100

    Ajout des fichiers de configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
-
+ 
+import { jqxChartComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxchart';
+ 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+ 
 export class AppComponent {
-  title = 'app works!';
+	sampleData: any = [
+        { Country: 'China', Population: 1347350000, Percent: 19.18 },
+        { Country: 'India', Population: 1210193422, Percent: 17.22 },
+        { Country: 'USA', Population: 313912000, Percent: 4.47 },
+        { Country: 'Indonesia', Population: 237641326, Percent: 3.38 },
+        { Country: 'France', Population: 60000000, Percent: 3.38 },
+        { Country: 'Brazil', Population: 192376496, Percent: 2.74 }
+    ];
+ 
+    padding: any = { left: 20, top: 5, right: 20, bottom: 5 };
+ 
+    titlePadding: any = { left: 90, top: 0, right: 0, bottom: 10 };
+ 
+    xAxis: any =
+    {
+        dataField: 'Country',
+        gridLines: { visible: true },
+        flip: false
+    };
+ 
+    valueAxis: any =
+    {
+        flip: true,
+        labels: {
+            visible: true,
+            formatFunction: (value: string) => {
+                return parseInt(value) / 1000000;
+            }
+        }
+    };
+ 
+    seriesGroups: any[] =
+    [
+        {
+            type: 'line',
+            orientation: 'vertical',
+            columnsGapPercent: 50,
+            toolTipFormatSettings: { thousandsSeparator: ',' },
+            series: [
+                { dataField: 'Population', displayText: 'Population (millions)' }
+            ]
+        }
+    ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
+ 
+import { jqxChartComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxchart';
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+	jqxChartComponent
   ],
   imports: [
     BrowserModule,
@@ -17,4 +19,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
