48dec01fe86d46b4d372d4b1bdcc98b0437c108c
Bernard BANITZ <bernard@banitz.fr>
2017-11-28 17:22:34 +0100

    essai affichage températures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,73 @@
 import { Component } from '@angular/core';
- 
-import { jqxChartComponent } from 'jqwidgets-framework/jqwidgets-ts/angular_jqxchart';
- 
+
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html'
 })
- 
+
 export class AppComponent {
-	sampleData: any = [
-        { Country: 'China', Population: 1347350000, Percent: 19.18 },
-        { Country: 'India', Population: 1210193422, Percent: 17.22 },
-        { Country: 'USA', Population: 313912000, Percent: 4.47 },
-        { Country: 'Indonesia', Population: 237641326, Percent: 3.38 },
-        { Country: 'France', Population: 60000000, Percent: 3.38 },
-        { Country: 'Brazil', Population: 192376496, Percent: 2.74 }
-    ];
- 
-    padding: any = { left: 20, top: 5, right: 20, bottom: 5 };
- 
-    titlePadding: any = { left: 90, top: 0, right: 0, bottom: 10 };
- 
+    months: string[] = ['Jan', 'Fev', 'Mar', 'Avr', 'Mai', 'Jun', 'Jul', 'Aou', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    source: any =
+    {
+        datatype: "csv",
+        datafields: [
+            { name: 'Date' },
+            { name: 'S&P 500' },
+            { name: 'NASDAQ' }
+        ],
+        url: '/sampledata/nasdaq_vs_sp500.txt'
+    };
+
+    dataAdapter: any = new jqx.dataAdapter(this.source, { async: false, autoBind: true, loadError: (xhr: any, status: any, error: any) => { alert('Error loading "' + this.source.url + '" : ' + error); } });
+
+    padding: any = { left: 10, top: 5, right: 10, bottom: 5 };
+
+    titlePadding: any = { left: 50, top: 0, right: 0, bottom: 10 };
+
     xAxis: any =
     {
-        dataField: 'Country',
-        gridLines: { visible: true },
-        flip: false
-    };
- 
-    valueAxis: any =
-    {
-        flip: true,
-        labels: {
+        dataField: 'Date',
+        formatFunction: (value: any) => {
+            return value.getDate() + '-' + this. months[value.getMonth()] + '-' + value.getFullYear();
+        },
+        type: 'date',
+        baseUnit: 'month',
+        valuesOnTicks: true,
+        minValue: '01-01-2014',
+        maxValue: '01-01-2015',
+        tickMarks: {
             visible: true,
-            formatFunction: (value: string) => {
-                return parseInt(value) / 1000000;
-            }
+            interval: 1,
+            color: '#BCBCBC'
+        },
+        unitInterval: 1,
+        gridLines: {
+            visible: true,
+            interval: 3,
+            color: '#BCBCBC'
+        },
+        labels: {
+            angle: -45,
+            rotationPoint: 'topright',
+            offset: { x: 0, y: -25 }
         }
     };
- 
-    seriesGroups: any[] =
+
+    valueAxis: any =
+    {
+        visible: true,
+        title: { text: 'Températures °Celsius<br>' },
+        tickMarks: { color: '#BCBCBC' }
+    };
+
+    seriesGroups: any =
     [
         {
             type: 'line',
-            orientation: 'vertical',
-            columnsGapPercent: 50,
-            toolTipFormatSettings: { thousandsSeparator: ',' },
             series: [
-                { dataField: 'Population', displayText: 'Population (millions)' }
+                { dataField: 'S&P 500', displayText: 'Température' },
+                { dataField: 'NASDAQ', displayText: 'Consigne' }
             ]
         }
     ];
